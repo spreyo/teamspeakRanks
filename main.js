a5603aa2c0007ea57411c3578a15cc942b8a97b5
spreyo <musicgamermail@gmail.com>
2023-12-03 02:25:25 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var app = express();
+const fs = require('fs')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const e = require('express');
+const fetch = (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+
+const { TeamSpeak, QueryProtocol, TeamSpeakChannel } = require('ts3-nodejs-library');
+const { channel } = require('diagnostics_channel');
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({ host: "sql1.revivenode.com", user: "u22477_HqoGSeGYhI", password: "xPW=GHEs6DEDN9f8!peFTIP9" })
+const teamspeak = new TeamSpeak({
+    host: "ts3.spreyo.xyz",
+    protocol: QueryProtocol.RAW,
+    serverport: 9987,
+    username: "serveradmin",
+    password: "spreyo1523",
+    nickname: "group manager"
+})
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+
+
+app.get('/', (req, res, next) => {
+    res.status(200);
+    res.send(('private api made by spreyo'));
+})
+
+app.post("/connectionTime", async (req, res, next) => {
+    const conn = await pool.getConnection()
+    const users = await teamspeak.clientList()
+    users.forEach(async user => {
+        if (user.type == 1) {
+            return
+        }
+        console.log(user.nickname)
+        const exists = await conn.query(`SELECT name, \`time\`
+        FROM s22477_ranks.ranks
+        WHERE name = "${user.uniqueIdentifier}";`)
+        console.log(exists)
+        if (exists.length > 0) {
+            let time = await conn.query(`SELECT time
+            FROM s22477_ranks.ranks
+            WHERE name='${user.uniqueIdentifier}'`)
+            time = time[0]["time"]
+            console.log(time)
+
+
+            await conn.query(`UPDATE s22477_ranks.ranks
+            SET time = ${time + 1}
+            WHERE name = '${user.uniqueIdentifier}'`)
+            time += 1;
+            let hours = Math.floor(time / 60);
+            let sgid = 0;
+            let groups = [15, 16, 17, 18, 19];
+            switch (hours) {
+                case 1:
+                    sgid = 15;
+                    break
+                case 5:
+                    sgid = 16;
+                    break
+                case 10:
+                    sgid = 17;
+                    break
+                case 20:
+                    sgid = 18;
+                    break
+                case 50:
+                    sgid = 19;
+                    break
+
+            }
+            console.log(sgid);
+            if (!user.servergroups.includes(sgid.toString())) {
+                groups.forEach(async group => {
+                    try {
+
+                        await user.delGroups(group);
+                    } catch (e) {
+                        console.log("group does not exist");
+                    }
+                })
+                await user.addGroups(sgid);
+
+            }
+
+
+
+
+
+        } else {
+            await conn.query(`INSERT INTO s22477_ranks.ranks
+            (name, time)
+            VALUES('${user.uniqueIdentifier}', 1);`)
+        }
+    })
+    // await conn.destroy();
+
+    res.send(200);
+
+})
+
+app.listen(3001, () => {
+    console.log('server running');
+})
