3cf1292817d54821c4a38adba375be1756486e19
spreyo <musicgamermail@gmail.com>
2023-12-09 21:43:27 +0100

    fix

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ app.post("/connectionTime", async (req, res, next) => {
                     } catch (e) {
                     }
                 })
-                // await user.addGroups(sgid);
+                await user.addGroups(sgid);
 
                 done = true;
             }
